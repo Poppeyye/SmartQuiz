6efd50b80520097a82d7f80de5a4cc72472fe62c
Poppeyye <alvaro.martinfidalgo@gmail.com>
2024-09-05 12:35:53 +0200

    add gunicorn to reqs

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from backend import create_app
 from flask_session import Session
 
 app = create_app()
-#Session(app)
+Session(app)
 
-#if __name__ == '__main__':
-#    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
