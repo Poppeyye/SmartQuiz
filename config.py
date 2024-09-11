377e3289a8ba22154d9f3172a45f5342a907de2f
Poppeyye <alvaro.martinfidalgo@gmail.com>
2024-09-11 13:39:58 +0200

    config env vars

--- config.py
+++ config.py
@@ -1,11 +1,19 @@
-from datetime import timedelta
 import os
+from datetime import timedelta
 from backend.models import db
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'supersecretkey'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:admin@localhost:5432/postgres'
-    #SQLALCHEMY_DATABASE_URI ='postgresql://ucbqf9cj16kupq:pcd4626e8098367ce91294c7871a51f0216a674026236b482ac6b427b3e5a2b4b@c724r43q8jp5nk.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com:5432/deb6497c3vcvd6'
+    
+    # Determinar el entorno actual (dev o pro)
+    ENV = os.environ.get('ENV', 'dev')  # Por defecto, será 'dev' si no se establece
+    
+    # Elegir la URI de la base de datos según el entorno
+    if ENV == 'pro':
+        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    else:
+        SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:admin@localhost:5432/postgres'
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SESSION_TYPE = 'sqlalchemy'
     SESSION_SQLALCHEMY = db
