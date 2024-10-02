bb81c9ba1192807401f3dd375b0185b31247b90b
Poppeyye <alvaro.martinfidalgo@gmail.com>
2024-10-02 12:09:06 +0200

    test

--- config.py
+++ config.py
@@ -13,8 +13,8 @@ class Config:
     PERMANENT_SESSION_LIFETIME = timedelta(days=30)
     JWT_SECRET_KEY = 'admin'
     JWT_TOKEN_LOCATION = ["cookies"]
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=60)
-    JWT_COOKIE_CSRF_PROTECT = False
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=1)
+    JWT_COOKIE_CSRF_PROTECT = True
     JWT_COOKIE_SECURE = True
     JWT_ACCESS_COOKIE_PATH = '/'
     JWT_REFRESH_COOKIE_PATH = '/refresh'
