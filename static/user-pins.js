ef9b2178921450b7bcaa86ba6bc73a2b96dc5b3f
Poppeyye <alvaro.martinfidalgo@gmail.com>
2024-10-09 18:13:37 +0200

    changes on UI

--- static/user-pins.js
+++ static/user-pins.js
@@ -1,27 +1,24 @@
 const pinDisplay = document.getElementById("pinDisplay");
-const toggleButton = document.getElementById("togglePin");
+const pinBox = document.getElementById("pinBox");
 
 pinDisplay.textContent = "****";
 
 export function togglePin() {
     if (pinDisplay.textContent === "****") {
         pinDisplay.textContent = pin_code; // Muestra el pin
-        toggleButton.textContent = "Ocultar PIN";
     } else {
         pinDisplay.textContent = "****"; // Oculta el pin
-        toggleButton.textContent = "Ver PIN";
     }
 }
 
-toggleButton.addEventListener('click', () => togglePin());
+pinDisplay.addEventListener('click', () => togglePin());
 
 export function mostrarPinSiDisponible(pin_code) {
     if (pin_code) {
-        toggleButton.style.display = "inline"; 
+        pinBox.style.display = "flex";
         pinDisplay.style.display = "inline"; 
         pinDisplay.textContent = "****"; 
     } else {
         pinDisplay.style.display = "none"; 
-        toggleButton.style.display = "none"; 
     }
 }
