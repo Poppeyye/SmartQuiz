4860130a798c703c3e1b10a20f77adee3064cd94
Poppeyye <alvaro.martinfidalgo@gmail.com>
2024-11-20 18:11:28 +0100

    extra security level

--- scripts/compress.js
+++ scripts/compress.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 const uglifyjs = require('uglify-js');
+const CleanCSS = require('clean-css');
+const JavaScriptObfuscator = require('javascript-obfuscator'); // Importar el obfuscador
 
 const inputDir = 'dist'; // Directorio de entrada
 const outputDir = 'static'; // Directorio de salida
@@ -15,7 +17,7 @@ function ensureDirectoryExistence(filePath) {
   fs.mkdirSync(dirname);
 }
 
-// Función para procesar la compresión y copia de archivos
+// Función para procesar la compresión, ofuscación y copia de archivos
 function processDirectory(inputDir, outputDir) {
   fs.readdirSync(inputDir).forEach(file => {
     const inputFilePath = path.join(inputDir, file);
@@ -26,16 +28,36 @@ function processDirectory(inputDir, outputDir) {
       processDirectory(inputFilePath, outputFilePath);
     } else {
       if (file.endsWith('.js')) {
-        // Minificar y escribir JS
-        const result = uglifyjs.minify(fs.readFileSync(inputFilePath, 'utf8'));
+        // Minificar JS
+        let code = fs.readFileSync(inputFilePath, 'utf8');
+        const minifiedResult = uglifyjs.minify(code);
         
-        if (result.error) {
-          console.error(`Error minifying ${file}: `, result.error);
+        if (minifiedResult.error) {
+          console.error(`Error minifying ${file}: `, minifiedResult.error);
+          return;
+        }
+        
+        // Ofuscar JS
+        const obfuscationResult = JavaScriptObfuscator.obfuscate(minifiedResult.code, {
+          compact: true, // Hacer el código más compacto
+          controlFlowFlattening: true, // Aumenta la ofuscación haciendo más complejo el flujo
+        });
+
+        ensureDirectoryExistence(outputFilePath);
+        fs.writeFileSync(outputFilePath, obfuscationResult.getObfuscatedCode(), 'utf8');
+        console.log(`Minified and obfuscated ${outputFilePath}`);
+      } else if (file.endsWith('.css')) {
+        // Minificar CSS
+        const inputCSS = fs.readFileSync(inputFilePath, 'utf8');
+        const outputCSS = new CleanCSS().minify(inputCSS);
+
+        if (outputCSS.errors.length > 0) {
+          console.error(`Error minifying CSS ${file}: `, outputCSS.errors);
           return;
         }
 
         ensureDirectoryExistence(outputFilePath);
-        fs.writeFileSync(outputFilePath, result.code, 'utf8');
+        fs.writeFileSync(outputFilePath, outputCSS.styles, 'utf8');
         console.log(`Minified ${outputFilePath}`);
       } else {
         // Copiar otros archivos
